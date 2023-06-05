3ea6f6f64431c7f68dca6be9f2bf16c4c2799ac4
Marvin Marks <marvin.marks@posteo.de>
2023-06-05 13:38:27 +0200

    Filter samples.sqlite3 into 3 different tables (dewolf_errors, samples, and summary)

--- filter.py
+++ filter.py
@@ -6,16 +6,17 @@ import sys
 from pathlib import Path
 from typing import Dict, Iterator, Union
 
-from pandas import DataFrame, read_sql_query
+from pandas import DataFrame, read_sql_query, unique
 
 
 class DBFilter:
-    QUERY = "SELECT * from dewolf"
+    QUERY = "SELECT * from dewolf" # query to read from samples.sqlite3
     SCHEMA = ""
 
     def __init__(self, df: DataFrame):
         self._summary = None
         self._filtered = None
+        self._samples = None
         self._df = df
 
     @classmethod
@@ -26,10 +27,25 @@ class DBFilter:
 
     @staticmethod
     def _write_df_to_sqlite3(df: DataFrame, database_path: Union[str, Path], table_name: str):
+        """
+        Write DataFrame to SQLite table
+        """
+        dtype_mapping = {
+            'int64': 'INTEGER',
+            'float64': 'REAL',
+            'bool': 'INTEGER',         # SQLite uses INTEGER for boolean values
+            'datetime64[ns]': 'TEXT',  # SQLite does not have a separate datetime type
+            'object': 'TEXT'           # For all other non-numeric types
+        }
+        dtype = {name: dtype_mapping.get(str(dt), "TEXT") for name, dt in df.dtypes.items()}
+        dtype["id"] = "INTEGER NOT NULL PRIMARY KEY"
         with sqlite3.connect(database_path) as con:
-            df.to_sql(table_name, con, index=False, if_exists="append")
+            df.sort_values("id").to_sql(table_name, con, index=False, if_exists="replace", dtype=dtype)
 
     def _get_summary(self) -> DataFrame:
+        """
+        Return DataFrame containing summary statistics over all samples
+        """
         commit = self._df.dewolf_current_commit.loc[0]  # just take any commit for now
         failed_runs = self._df[self._df.is_successful == 0]
         summary = {
@@ -40,6 +56,7 @@ class DBFilter:
             "total_errors": len(failed_runs),
             "unique_exceptions": len(failed_runs.dewolf_exception.unique()),
             "unique_tracebacks": len(failed_runs.dewolf_traceback.unique()),
+            "processed_samples": len(self._df.sample_hash.unique()),
         }
         return DataFrame(summary, index=[0])
 
@@ -48,34 +65,57 @@ class DBFilter:
         Filter dataset to contain 10 smallest cases per unique exception AND traceback
         generate case id for semantic grouping of similar errors. (e.g., ExceptionType@file.py:42)
         """
-        f = lambda x: x.nsmallest(10, "function_basic_block_count")
-        failed_runs = self._df[self._df.is_successful == 0]
-        exception_counts = failed_runs["dewolf_exception"].value_counts()
-        failed_runs["exception_count_pre_filter"] = failed_runs["dewolf_exception"].map(exception_counts)
-        filtered_df = failed_runs.groupby(["dewolf_exception", "dewolf_traceback"]).apply(f)
+        failed_runs = self._df[self._df.is_successful == 0].copy()
+        # enrich dewolf errors data
         failed_runs["error_file_path"] = failed_runs["dewolf_traceback"].apply(self._get_last_file_path)
         failed_runs["error_line"] = failed_runs["dewolf_traceback"].apply(self._get_last_line_number)
         # case id: ExceptionType@file.py:42
         failed_runs["case_group"] = (
-            failed_runs["dewolf_exception"].str.split().str[0].str.strip(": ")
-            + "@"
-            + failed_runs["error_file_path"].str.split("/").str[-1]
-            + ":"
-            + failed_runs["error_line"]
-        )
+                failed_runs["dewolf_exception"].str.split().str[0].str.strip(": ")
+                + "@"
+                + failed_runs["error_file_path"].str.split("/").str[-1]
+                + ":"
+                + failed_runs["error_line"]
+                )
+        errors_per_group_count = failed_runs["case_group"].value_counts()
+        failed_runs["errors_per_group_count_pre_filter"] = failed_runs["case_group"].map(errors_per_group_count)
+        # filter n smallest unique per exception and traceback
+        f = lambda x: x.nsmallest(10, "function_basic_block_count")
+        filtered_df = failed_runs.groupby(["dewolf_exception", "dewolf_traceback"]).apply(f)
         assert isinstance(filtered_df, DataFrame)
         return filtered_df.reset_index(drop=True)
+
+    def _get_samples(self) -> DataFrame:
+        """
+        Return DataFrame containing per sample statistics
+        """
+        # TODO deduplicate multiple runs on the same sample
+        unique_lst_to_str = lambda x: ",".join(iter(unique(x)))
+        samples = (
+            self._df.groupby("sample_hash")
+            .agg(
+                platform=("function_platform", unique_lst_to_str),
+                commit=("dewolf_current_commit", unique_lst_to_str),
+                binaryninja_version=("binaryninja_version", unique_lst_to_str),
+                count_error=("is_successful", lambda x: sum(x == 0)),
+                count_success=("is_successful", lambda x: sum(x == 1)),
+                count_total_processed=("is_successful", "count"),
+            )
+            .reset_index()
+        )
+        samples["id"] = samples.index
+        return samples
 
     @staticmethod
     def _get_last_file_path(traceback: str) -> str:
         """Extract the last file path contained in a Traceback"""
-        file_path, line, *_ = traceback.rpartition("File ")[-1].split(", ")
+        file_path, _, *_ = traceback.rpartition("File ")[-1].split(", ")
         return file_path.strip('"')
 
     @staticmethod
     def _get_last_line_number(traceback: str) -> str:
         """Return the line number (as str) from the last row of a Traceback"""
-        file_path, line, *_ = traceback.rpartition("File ")[-1].split(", ")
+        _, line, *_ = traceback.rpartition("File ")[-1].split(", ")
         return line.lstrip("line ")
 
     @property
@@ -90,16 +130,24 @@ class DBFilter:
             self._filtered = self._get_filtered()
         return self._filtered
 
+    @property
+    def samples(self) -> DataFrame:
+        if self._samples is None:
+            self._samples = self._get_samples()
+        return self._samples
+
     def write(self, file_path: Path):
         self._write_df_to_sqlite3(self.summary, file_path, "summary")
-        self._write_df_to_sqlite3(self.filtered, file_path, "dewolf")
+        self._write_df_to_sqlite3(self.filtered, file_path, "dewolf_errors")
+        self._write_df_to_sqlite3(self.samples, file_path, "samples")
+
 
 def print_sample_hashes(db_file: Path):
     """Print all sample hashes contained in DB file"""
     sample_hashes = set()
     COLUMN = "sample_hash"
     with sqlite3.connect(db_file) as con:
-        cursor =  con.cursor()
+        cursor = con.cursor()
         cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
         table_names = cursor.fetchall()
         for table in table_names:
@@ -114,6 +162,7 @@ def print_sample_hashes(db_file: Path):
         cursor.close()
     for s in sample_hashes:
         print(s)
+
 
 def existing_file(path):
     """Check if the provided path is an existing file."""
